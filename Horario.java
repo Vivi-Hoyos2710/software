dc0c7be6257282c845b5db5a49b986f7e9c5f23a
Vivi-Hoyos2710 <vhoyoss@eafit.edu.co>
2021-04-28 18:12:08 -0500

    Creacion clase horario

---
+++ Horario.java
@@ -0,0 +1,23 @@
+public class Horario {
+    private int dias;
+    private int mes;
+    private int hora;
+    private Actividad[][][] temporalidad= new Actividad[12][][24];
+    temporalidad[0][31];
+    temporalidad[1][28];
+    temporalidad[2][31];
+    temporalidad[3][30];
+    temporalidad[4][31];
+    temporalidad[5][30];
+    temporalidad[6][31];
+    temporalidad[7][31];
+    temporalidad[8][30];
+    temporalidad[9][31];
+    temporalidad[10][30];
+    temporalidad[11][31];
+    public Horario(int m, int d, int h){
+        this.mes=m;
+        this.dias=d;
+        this.hora=h;
+    }
+}
